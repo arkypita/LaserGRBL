f6b3edbbde723b6823397be5f6f6dd6f723f2c6f
arkypita <arkypita@bergamo3.it>
2016-12-05 17:26:57 +0100

    Add dockingmanager library for future UI refactoring

---
+++ LaserGRBL/UserControls/DockingManager/Docking/DummyControl.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Windows.Forms;
+
+namespace LaserGRBL.UserControls.DockingManager
+{
+	internal class DummyControl : Control
+	{
+		public DummyControl()
+		{
+			SetStyle(ControlStyles.Selectable, false);
+		}
+	}
+}
