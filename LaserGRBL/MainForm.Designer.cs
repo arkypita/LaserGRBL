72faf758a3b6c501339ba92eca52aa2a7192fd12
arkypita <arkypita@bergamo3.it>
2016-12-16 15:02:03 +0100

    Program now run Maximized

--- LaserGRBL/MainForm.Designer.cs
+++ LaserGRBL/MainForm.Designer.cs
@@ -286,7 +286,6 @@
 			// 
 			// DockArea
 			// 
-			this.DockArea.ActiveAutoHideContent = null;
 			this.DockArea.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.DockArea.DockBackColor = System.Drawing.SystemColors.Control;
 			this.DockArea.Location = new System.Drawing.Point(0, 0);
@@ -299,6 +298,7 @@
 			tabGradient1.StartColor = System.Drawing.SystemColors.Control;
 			tabGradient1.TextColor = System.Drawing.SystemColors.ControlDarkDark;
 			autoHideStripSkin1.TabGradient = tabGradient1;
+			autoHideStripSkin1.TextFont = new System.Drawing.Font("Segoe UI", 9F);
 			dockPanelSkin1.AutoHideStripSkin = autoHideStripSkin1;
 			tabGradient2.EndColor = System.Drawing.SystemColors.ControlLightLight;
 			tabGradient2.StartColor = System.Drawing.SystemColors.ControlLightLight;
@@ -312,6 +312,7 @@
 			tabGradient3.TextColor = System.Drawing.SystemColors.ControlText;
 			dockPaneStripGradient1.InactiveTabGradient = tabGradient3;
 			dockPaneStripSkin1.DocumentGradient = dockPaneStripGradient1;
+			dockPaneStripSkin1.TextFont = new System.Drawing.Font("Segoe UI", 9F);
 			tabGradient4.EndColor = System.Drawing.SystemColors.ActiveCaption;
 			tabGradient4.LinearGradientMode = System.Drawing.Drawing2D.LinearGradientMode.Vertical;
 			tabGradient4.StartColor = System.Drawing.SystemColors.GradientActiveCaption;
@@ -352,6 +353,7 @@
 			this.MainMenuStrip = this.MMn;
 			this.Name = "MainForm";
 			this.Text = "Laser GRBL";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFormFormClosing);
 			this.Load += new System.EventHandler(this.MainForm_Load);
 			this.StatusBar.ResumeLayout(false);
