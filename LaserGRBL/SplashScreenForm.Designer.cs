2ab9a59e5f56ffa9be77ad2c70e83efdcac3604f
arkypita <arkypita@bergamo3.it>
2017-01-07 23:31:48 +0100

    New version Logo (Rhydon)

--- LaserGRBL/SplashScreenForm.Designer.cs
+++ LaserGRBL/SplashScreenForm.Designer.cs
@@ -47,7 +47,7 @@
 			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
 			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(421, 187);
+			this.pictureBox1.Size = new System.Drawing.Size(429, 195);
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
 			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
@@ -59,7 +59,7 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.White;
 			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-			this.ClientSize = new System.Drawing.Size(421, 187);
+			this.ClientSize = new System.Drawing.Size(429, 195);
 			this.Controls.Add(this.pictureBox1);
 			this.DoubleBuffered = true;
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
