ea4472515b84f3ed3a91cdc56d3810bf83df4481
arkypita <arkypita@bergamo3.it>
2017-09-19 11:18:24 +0200

    project file reorganization

--- LaserGRBL/UI/UserControls/ProgressBar/ColorProgressBar.cs
+++ LaserGRBL/UI/UserControls/ProgressBar/ColorProgressBar.cs
@@ -517,6 +517,34 @@ namespace LaserGRBL.UserControls
 
 
 	}
+
+
+
+	public class ColorProgressBarDesigner : System.Windows.Forms.Design.ControlDesigner
+	{
+
+		public ColorProgressBarDesigner()
+		{
+		}
+		//New
+
+
+		// clean up some unnecessary properties
+		protected override void PostFilterProperties(IDictionary Properties)
+		{
+			Properties.Remove("AllowDrop");
+			Properties.Remove("BackgroundImage");
+			Properties.Remove("ContextMenu");
+			Properties.Remove("FlatStyle");
+			Properties.Remove("Image");
+			Properties.Remove("ImageAlign");
+			Properties.Remove("ImageIndex");
+			Properties.Remove("ImageList");
+			Properties.Remove("Text");
+			Properties.Remove("TextAlign");
+		}
+		//PostFilterProperties
+	}
 }
 
 //=======================================================
