0efc418bfdf994b0413bc14bdbaa7a04b504c5bc
arkypita <arkypita@bergamo3.it>
2018-01-09 23:59:52 +0100

    some logging in com

--- LaserGRBL/ComWrapper/UsbSerial.cs
+++ LaserGRBL/ComWrapper/UsbSerial.cs
@@ -32,6 +32,7 @@ namespace LaserGRBL.ComWrapper
 					com.NewLine = "\n";
 					com.WriteTimeout = 1000; //se si blocca in write
 
+					//log("com", string.Format("Open {0} @ {1} baud", com.PortName.ToUpper(), com.BaudRate));
 					Logger.LogMessage("OpenCom", "Open {0} @ {1} baud", com.PortName.ToUpper(), com.BaudRate);
 
 					com.Open();
@@ -62,6 +63,8 @@ namespace LaserGRBL.ComWrapper
 		{
 			if (com.IsOpen)
 			{
+
+				//log("com", string.Format("Close {0} [{1}]", com.PortName.ToUpper(), auto ? "CORE" : "USER"));
 				Logger.LogMessage("CloseCom", "Close {0} [{1}]", com.PortName.ToUpper(), auto ? "CORE" : "USER");
 				com.DiscardOutBuffer();
 				com.DiscardInBuffer();
@@ -73,15 +76,37 @@ namespace LaserGRBL.ComWrapper
 		{get { return com.IsOpen; }}
 
 		public void Write(byte b)
-		{com.Write(new byte[]{ b } , 0, 1);}
+		{
+			//log("tx", string.Format("[0x{0:X}]", b));
+			com.Write(new byte[]{ b } , 0, 1);
+		}
 
 		public void Write(string text)
-		{com.Write(text);}
+		{
+			//log("tx", text);
+			com.Write(text);
+		}
 
+		int logcnt = 0;
+		
 		public string ReadLineBlocking()
-		{return com.ReadLine();} //la lettura della com è bloccante per natura
+		{
+			return com.ReadLine();
+			//string rv = com.ReadLine();
+			//log("rx", rv);
+			//return rv;
+
+		} //la lettura della com è bloccante per natura
 
 		public bool HasData()
 		{ return com.BytesToRead > 0; }
+
+		private void log(string operation, string line)
+		{
+			line = line.Replace("\r", "\\r");
+			line = line.Replace("\n", "\\n");
+			try { System.IO.File.AppendAllText(System.IO.Path.Combine(GrblCore.DataPath, string.Format("comlog.txt", operation)), string.Format("{0:00000000}\t{1:00000}\t{2}\t{3}\r\n", Tools.TimingBase.TimeFromApplicationStartup().TotalMilliseconds, logcnt++, operation, line)); }
+			catch { }
+		}
 	}
 }
