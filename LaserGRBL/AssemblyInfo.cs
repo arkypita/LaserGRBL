7ed8a96a3032028ac7ff60b61ba5bc4275273f2d
arkypita <arkypita@bergamo3.it>
2018-06-14 13:11:59 +0200

    enhanced custom buttons

--- LaserGRBL/AssemblyInfo.cs
+++ LaserGRBL/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Resources;
 // È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build 
 // utilizzando l'asterisco (*) come descritto di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("2.8.48")]
+[assembly: AssemblyVersion ("2.8.49")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
