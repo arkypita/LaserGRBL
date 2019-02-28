78715da1de5e661c0e28cbf6ea2c378f284644b3
arkypita <arkypita@bergamo3.it>
2019-02-28 18:53:33 +0100

    Add support for Smoothie

--- LaserGRBL/JogForm.Designer.cs
+++ LaserGRBL/JogForm.Designer.cs
@@ -28,84 +28,84 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.TT = new System.Windows.Forms.ToolTip(this.components);
-			this.UpdateFMax = new System.Windows.Forms.Timer(this.components);
-			this.LblSpeed = new System.Windows.Forms.Label();
-			this.TbSpeed = new System.Windows.Forms.TrackBar();
-			this.LeftPanel = new System.Windows.Forms.Panel();
-			this.move2DControl1 = new LaserGRBL.UserControls.Move2DControl();
-			((System.ComponentModel.ISupportInitialize)(this.TbSpeed)).BeginInit();
-			this.LeftPanel.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// UpdateFMax
-			// 
-			this.UpdateFMax.Interval = 1000;
-			this.UpdateFMax.Tick += new System.EventHandler(this.UpdateFMax_Tick);
-			// 
-			// LblSpeed
-			// 
-			this.LblSpeed.AutoSize = true;
-			this.LblSpeed.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.LblSpeed.Location = new System.Drawing.Point(0, 112);
-			this.LblSpeed.Name = "LblSpeed";
-			this.LblSpeed.Size = new System.Drawing.Size(37, 13);
-			this.LblSpeed.TabIndex = 17;
-			this.LblSpeed.Text = "F1000";
-			// 
-			// TbSpeed
-			// 
-			this.TbSpeed.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.TbSpeed.LargeChange = 100;
-			this.TbSpeed.Location = new System.Drawing.Point(0, 0);
-			this.TbSpeed.Maximum = 4000;
-			this.TbSpeed.Minimum = 10;
-			this.TbSpeed.Name = "TbSpeed";
-			this.TbSpeed.Orientation = System.Windows.Forms.Orientation.Vertical;
-			this.TbSpeed.Size = new System.Drawing.Size(42, 112);
-			this.TbSpeed.SmallChange = 50;
-			this.TbSpeed.TabIndex = 16;
-			this.TbSpeed.TickFrequency = 200;
-			this.TbSpeed.TickStyle = System.Windows.Forms.TickStyle.Both;
-			this.TbSpeed.Value = 1000;
-			this.TbSpeed.ValueChanged += new System.EventHandler(this.TbSpeed_ValueChanged);
-			this.TbSpeed.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnSliderMouseUP);
-			// 
-			// LeftPanel
-			// 
-			this.LeftPanel.Controls.Add(this.TbSpeed);
-			this.LeftPanel.Controls.Add(this.LblSpeed);
-			this.LeftPanel.Dock = System.Windows.Forms.DockStyle.Left;
-			this.LeftPanel.Location = new System.Drawing.Point(0, 0);
-			this.LeftPanel.Name = "LeftPanel";
-			this.LeftPanel.Size = new System.Drawing.Size(42, 125);
-			this.LeftPanel.TabIndex = 1;
-			// 
-			// move2DControl1
-			// 
-			this.move2DControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.move2DControl1.Location = new System.Drawing.Point(42, 0);
-			this.move2DControl1.Name = "move2DControl1";
-			this.move2DControl1.Padding = new System.Windows.Forms.Padding(2);
-			this.move2DControl1.Size = new System.Drawing.Size(192, 125);
-			this.move2DControl1.TabIndex = 2;
-			this.move2DControl1.MoveClick += new LaserGRBL.UserControls.Move2DControl.MoveEventHandler(this.Move_Click);
-			this.move2DControl1.HomeClick += new System.EventHandler(this.Home_Click);
-			// 
-			// JogForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.move2DControl1);
-			this.Controls.Add(this.LeftPanel);
-			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.Name = "JogForm";
-			this.Size = new System.Drawing.Size(234, 125);
-			((System.ComponentModel.ISupportInitialize)(this.TbSpeed)).EndInit();
-			this.LeftPanel.ResumeLayout(false);
-			this.LeftPanel.PerformLayout();
-			this.ResumeLayout(false);
+            this.components = new System.ComponentModel.Container();
+            this.TT = new System.Windows.Forms.ToolTip(this.components);
+            this.UpdateFMax = new System.Windows.Forms.Timer(this.components);
+            this.LblSpeed = new System.Windows.Forms.Label();
+            this.TbSpeed = new System.Windows.Forms.TrackBar();
+            this.LeftPanel = new System.Windows.Forms.Panel();
+            this.move2DControl1 = new LaserGRBL.UserControls.Move2DControl();
+            ((System.ComponentModel.ISupportInitialize)(this.TbSpeed)).BeginInit();
+            this.LeftPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // UpdateFMax
+            // 
+            this.UpdateFMax.Interval = 1000;
+            this.UpdateFMax.Tick += new System.EventHandler(this.UpdateFMax_Tick);
+            // 
+            // LblSpeed
+            // 
+            this.LblSpeed.AutoSize = true;
+            this.LblSpeed.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.LblSpeed.Location = new System.Drawing.Point(0, 112);
+            this.LblSpeed.Name = "LblSpeed";
+            this.LblSpeed.Size = new System.Drawing.Size(37, 13);
+            this.LblSpeed.TabIndex = 17;
+            this.LblSpeed.Text = "F1000";
+            // 
+            // TbSpeed
+            // 
+            this.TbSpeed.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TbSpeed.LargeChange = 100;
+            this.TbSpeed.Location = new System.Drawing.Point(0, 0);
+            this.TbSpeed.Maximum = 4000;
+            this.TbSpeed.Minimum = 10;
+            this.TbSpeed.Name = "TbSpeed";
+            this.TbSpeed.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.TbSpeed.Size = new System.Drawing.Size(45, 112);
+            this.TbSpeed.SmallChange = 50;
+            this.TbSpeed.TabIndex = 16;
+            this.TbSpeed.TickFrequency = 200;
+            this.TbSpeed.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.TbSpeed.Value = 1000;
+            this.TbSpeed.ValueChanged += new System.EventHandler(this.TbSpeed_ValueChanged);
+            this.TbSpeed.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnSliderMouseUP);
+            // 
+            // LeftPanel
+            // 
+            this.LeftPanel.Controls.Add(this.TbSpeed);
+            this.LeftPanel.Controls.Add(this.LblSpeed);
+            this.LeftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftPanel.Location = new System.Drawing.Point(0, 0);
+            this.LeftPanel.Name = "LeftPanel";
+            this.LeftPanel.Size = new System.Drawing.Size(42, 125);
+            this.LeftPanel.TabIndex = 1;
+            // 
+            // move2DControl1
+            // 
+            this.move2DControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.move2DControl1.Location = new System.Drawing.Point(42, 0);
+            this.move2DControl1.Name = "move2DControl1";
+            this.move2DControl1.Padding = new System.Windows.Forms.Padding(2);
+            this.move2DControl1.Size = new System.Drawing.Size(192, 125);
+            this.move2DControl1.TabIndex = 2;
+            this.move2DControl1.MoveClick += new LaserGRBL.UserControls.Move2DControl.MoveEventHandler(this.Move_Click);
+            this.move2DControl1.HomeClick += new System.EventHandler(this.Home_Click);
+            // 
+            // JogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.move2DControl1);
+            this.Controls.Add(this.LeftPanel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "JogForm";
+            this.Size = new System.Drawing.Size(234, 125);
+            ((System.ComponentModel.ISupportInitialize)(this.TbSpeed)).EndInit();
+            this.LeftPanel.ResumeLayout(false);
+            this.LeftPanel.PerformLayout();
+            this.ResumeLayout(false);
 
 		}
 
---
+++ LaserGRBL/SmoothieCore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LaserGRBL
+{
+    public class SmoothieCore : GrblCore
+    {
+        public SmoothieCore(System.Windows.Forms.Control syncroObject, PreviewForm cbform) : base(syncroObject, cbform)
+        {
+        }
+
+        protected override void InitializeBoard()
+        {
+            SendImmediate(10); //send a new line
+            base.InitializeBoard();
+        }
+
+        protected override void ParseF(string p)
+        {
+            string sfs = p.Substring(2, p.Length - 2);
+            string[] fs = sfs.Split(",".ToCharArray());
+            SetFS(ParseFloat(fs[0]), ParseFloat(fs[1]));
+        }
+
+        public override bool SupportJogging => false;
+    }
+
+}
