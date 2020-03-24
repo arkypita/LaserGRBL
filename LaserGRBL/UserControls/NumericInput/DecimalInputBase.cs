ba10b94bd36179b65a99d5c78af15fc3a7adf1e7
arkypita <arkypita@users.noreply.github.com>
2020-03-24 15:40:07 +0100

    add copyright info in files

--- LaserGRBL/UserControls/NumericInput/DecimalInputBase.cs
+++ LaserGRBL/UserControls/NumericInput/DecimalInputBase.cs
@@ -1,4 +1,10 @@
-﻿using System.ComponentModel;
+﻿//Copyright (c) 2016-2020 Diego Settimi - https://github.com/arkypita/
+
+// This program is free software; you can redistribute it and/or modify  it under the terms of the GPLv3 General Public License as published by  the Free Software Foundation; either version 3 of the License, or (at  your option) any later version.
+// This program is distributed in the hope that it will be useful, but  WITHOUT ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GPLv3  General Public License for more details.
+// You should have received a copy of the GPLv3 General Public License  along with this program; if not, write to the Free Software  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,  USA. using System;
+
+using System.ComponentModel;
 using System.Drawing;
 using System;
 
