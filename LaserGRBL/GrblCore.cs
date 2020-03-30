3b591f2214a3458a37a3a137ac22b96ea1f079e0
arkypita <arkypita@users.noreply.github.com>
2020-03-30 12:18:42 +0200

    Update GrblCore.cs

--- LaserGRBL/GrblCore.cs
+++ LaserGRBL/GrblCore.cs
@@ -20,7 +20,7 @@ namespace LaserGRBL
 	public class GrblCore
 	{
 		public static string GCODE_STD_HEADER = "G90 (use absolute coordinates)";
-		public static string GCODE_STD_PASSES = "G91\r\nG0 Z-1 (sinks the Z axis)\r\nG90";
+		public static string GCODE_STD_PASSES = ";(Uncomment if you want to sink Z axis)\r\n;G91 (use relative coordinates)\r\n;G0 Z-1 (sinks the Z axis, 1mm)\r\n;G90 (use absolute coordinates)";
 		public static string GCODE_STD_FOOTER = "G0 X0 Y0 Z0 (move back to origin)";
 
 		[Serializable]
