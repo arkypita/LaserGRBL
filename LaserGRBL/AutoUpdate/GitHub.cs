411a500e6c54e8cd90a70cd1c1986fbcdc6b677e
arkypita <arkypita@users.noreply.github.com>
2020-04-16 12:01:21 +0200

    enable "auto update build" on 2%

--- LaserGRBL/AutoUpdate/GitHub.cs
+++ LaserGRBL/AutoUpdate/GitHub.cs
@@ -155,7 +155,20 @@ namespace LaserGRBL
 
 		public static void InitUpdate()
 		{
+			InitFlags();
 			CleanupOldVersion();
+		}
+
+		private static void InitFlags()
+		{
+			if (!Settings.ExistObject("Auto Update Build"))
+			{
+				int percentage = 2; //enable "auto update build" on 2% of installation to be used as test platform
+
+				Random rng = new Random();
+				Settings.SetObject("Auto Update Build", Settings.GetObject("Auto Update", true) && rng.Next(0, 100) < percentage); 
+				Settings.Save();
+			}
 		}
 
 		private static void CleanupOldVersion()
