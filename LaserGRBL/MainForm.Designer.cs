f68ad8e21af7503f085456b2e1a3914384264322
arkypita <arkypita@bergamo3.it>
2022-01-04 21:21:14 +0100

    Update romanian flag

--- LaserGRBL/MainForm.Designer.cs
+++ LaserGRBL/MainForm.Designer.cs
@@ -105,7 +105,7 @@
 			this.polishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.greekToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.turkishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.romanianToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.romanianToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.installCH340DriverToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.flashGrblFirmwareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -676,12 +676,13 @@
 			resources.ApplyResources(this.turkishToolStripMenuItem, "turkishToolStripMenuItem");
 			this.turkishToolStripMenuItem.Name = "turkishToolStripMenuItem";
 			this.turkishToolStripMenuItem.Click += new System.EventHandler(this.turkishToolStripMenuItem_Click);
-            // 
-            // romanianToolStripMenuItem
-            // 
-            resources.ApplyResources(this.romanianToolStripMenuItem, "romanianToolStripMenuItem");
-            this.romanianToolStripMenuItem.Name = "romanianToolStripMenuItem";
-            this.romanianToolStripMenuItem.Click += new System.EventHandler(this.romanianToolStripMenuItem_Click);
+			// 
+			// romanianToolStripMenuItem
+			// 
+			resources.ApplyResources(this.romanianToolStripMenuItem, "romanianToolStripMenuItem");
+			this.romanianToolStripMenuItem.Name = "romanianToolStripMenuItem";
+			this.romanianToolStripMenuItem.Click += new System.EventHandler(this.romanianToolStripMenuItem_Click);
+			// 
 			// toolsToolStripMenuItem
 			// 
 			this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
