e1676bc61153ff21e60713d220c3bd7153f6e165
arkypita <arkypita@users.noreply.github.com>
2024-05-28 18:06:19 +0200

    Add event handler

--- LaserGRBL/UserControls/TextInput.Designer.cs
+++ LaserGRBL/UserControls/TextInput.Designer.cs
@@ -28,29 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.mTextBox = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // mTextBox
-            // 
-            this.mTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this.mTextBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// mTextBox
+			// 
+			this.mTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.mTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.mTextBox.Location = new System.Drawing.Point(4, 4);
-            this.mTextBox.Name = "mTextBox";
-            this.mTextBox.Size = new System.Drawing.Size(92, 13);
-            this.mTextBox.TabIndex = 0;
-            // 
-            // TextInput
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.mTextBox);
-            this.Name = "TextInput";
-            this.Size = new System.Drawing.Size(100, 20);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.mTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.mTextBox.Location = new System.Drawing.Point(4, 4);
+			this.mTextBox.Name = "mTextBox";
+			this.mTextBox.Size = new System.Drawing.Size(92, 13);
+			this.mTextBox.TabIndex = 0;
+			this.mTextBox.TextChanged += new System.EventHandler(this.mTextBox_TextChanged);
+			// 
+			// TextInput
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.mTextBox);
+			this.Name = "TextInput";
+			this.Size = new System.Drawing.Size(100, 20);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
